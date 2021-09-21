9877cffef3f70f7512fbaf43d841e4216587ee79
Hyunsoo <laugh4mile@naver.com>
2021-09-22 02:06:38 +0900

    스프링부트 강좌 14강 까지

--- src/main/java/com/cos/blog/test/BlogController.java
+++ src/main/java/com/cos/blog/test/BlogController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 //스프링이 com.cos.blog 패키지 이하를 스캔해서 모든 파일ㄴㄴ 
 //특정 어노테이션이 붙은 클래스 파일들을 new해서(IoC) 스프링 컨테이너에 관리해 줍니다. 
 @RestController 
-public class BlogControllerTest {
+public class BlogController {
 	
 	//http://localhost:8080/test/hello
 	@GetMapping("/test/hello")
---
+++ src/main/java/com/cos/blog/test/HttpController.java
@@ -0,0 +1,45 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+//사용자가 요청 -> 응답(HTML 파일)
+
+// 사용자의 요청 -> 응답(data) 
+@RestController
+public class HttpController {
+	
+	//인터넷 브라우저 요청은 무조건 get 요청밖에 할 수 없다.
+	//http://localhost:8080/http/get (select)
+	// 데이터를 get방식으로 요청하는 방법은 쿼리 스트링밖에 없다.
+		// id=1&username=ssar&password=1234&email=ssar@nate.com 
+	@GetMapping("/http/get") 
+	public String getTest(@RequestBody Member m) {
+		return "get 요청 : "+m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
+	}
+	 
+	//http://localhost:8080/http/post (insert)
+	@PostMapping("/http/post")
+	public String postTest(@RequestBody  Member m) {
+		return "post 요청 : "+m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
+	}
+	
+	//http://localhost:8080/http/put (put)
+	@PutMapping("/http/put")
+	public String putTest(@RequestBody Member m) {
+		return "put 요청 : "+m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail() ;
+	}
+	
+	//http://localhost:8080/http/delete (delete)
+	@DeleteMapping("/http/delete")
+	public String deleteTest() {
+		return "delete 요청";
+	}
+	
+}
