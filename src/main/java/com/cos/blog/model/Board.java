8967942b026a41a302faa4f65d8c9d461e8956cb
Hyunsoo <laugh4mile@naver.com>
2021-09-25 10:17:49 +0900

    Board 테이블 생성. User를 참조하는 외래키도 한방에!

--- src/main/java/com/cos/blog/model/Board.java
+++ src/main/java/com/cos/blog/model/Board.java
@@ -1,5 +1,48 @@
 package com.cos.blog.model;
 
-public class Board {
+import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Board {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+	private int id;
+	
+	@Column(nullable = false, length = 100)
+	private String title;
+	
+	@Lob // 대용량 데이터
+	private String content; // 섬머노트 라이브러리를 쓸거임 // <html>태그가 섞여서 디자인 됨 -> 겁나 길어짐
+	
+	@ColumnDefault("0")
+	private int count; // 조회수
+	
+	@ManyToOne // Board가 Many User는 One // 한명의 유저는 여러개의 게시글을 쓸 수 있다는 거임.
+	@JoinColumn(name = "userId") // 실제로 db에 만들어질 때는 userId로 만들어 질거임
+	private User userId; // DataBase는 오브젝트를 저장할 수 없다. 그래서 FK를 사용하지. 하지만 자바는 오브젝트 저장이 가능함. 
+	
+	@CreationTimestamp // 데이터가 insert 혹은 update 될때 자동으로 값이 들어가는 꿀 어노테이션임!
+	private Timestamp createDate;
 }
--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -11,7 +11,16 @@ import javax.persistence.Id;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 // ORM -> Java(혹은 다른 언어) Object -> 테이블로 매핑해주는 기술
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity // User 클래스가 MySQL에 테이블이 생성된다.
 public class User {
 	@Id  // 기본 키가 됨
