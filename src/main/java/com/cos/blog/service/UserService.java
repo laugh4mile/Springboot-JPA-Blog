58d8c4f2fa2e1523518778988d3f8fa2ec8da686
Hyunsoo <laugh4mile@naver.com>
2021-10-10 21:31:16 +0900

    userService 생성 + 회원가입

---
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -0,0 +1,30 @@
+package com.cos.blog.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.cos.blog.model.User;
+import com.cos.blog.repository.UserRepository;
+
+// @Service 어노테이션이 붙으면 스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. IoC를 해준다는 의미!(메모리에 띄워준다!)
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+//	@PostMapping
+	@Transactional
+	public int 회원가입(User user) {
+		try {
+			userRepository.save(user);
+			return 1;
+		} catch (Exception e) {
+			System.out.println("UserService : 회원가입() : "+e.getMessage());
+		}
+		return -1;
+	}
+}
