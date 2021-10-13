0ce228f61dfca56b0c4ba72793164990284b3ae6
Hyunsoo <laugh4mile@naver.com>
2021-10-13 20:56:26 +0900

    @Autowired 를 이용하여 HttpSession을 bean으로 미리 등록해서 DI할 수도 있다.

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -18,6 +18,9 @@ public class UserApiController {
 
 	@Autowired
 	private UserService userService;
+
+	@Autowired // 세션 객체를 스프링 컨테이너가 bean으로 가지고 있을 수 있다.
+	private HttpSession session; 
 	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) {
@@ -27,7 +30,7 @@ public class UserApiController {
 	}
 	
 	@PostMapping("/api/user/login")
-	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) { // 세션은 컨트롤러의 매개변수로 받을 수 있다.
+	public ResponseDto<Integer> login(@RequestBody User user) {
 //		user.setRole(RoleType.USER);
 		User principal = userService.로그인(user); // principal = 접근주체라는 뜻
 		
