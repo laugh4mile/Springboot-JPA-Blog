eca7729ffff2a4c745c8dc2f6baae46a6603e07e
Hyunsoo <laugh4mile@naver.com>
2021-10-16 01:05:17 +0900

    SpringSecurity는 csrf 토큰이 없으면 기본적으로 요청을 막기때문에 일단 테스트를 위해 disable()를 적용.

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	}
 	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	protected void configure(HttpSecurity http) throws Exception { 
 		http
 			.csrf().disable() // csrf 토큰 비활성화 (테스트시 걸어두는게 좋음)
 			.authorizeRequests() // request가 들어오면
